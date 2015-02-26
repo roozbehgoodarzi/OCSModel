e337808eac7769779912f30e5eef031667891dae
Roozbeh <roozbeh.goodarzi@gmail.com>
2015-02-26 11:16:03 +0330

    Person added

--- src/main/java/com/ocs/config/AppConfig.java
+++ src/main/java/com/ocs/config/AppConfig.java
@@ -2,14 +2,13 @@ package com.ocs.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * Created by Goodarzi on 12/18/14.
  */
 @Configuration
 @ComponentScan({"com.ocs","com.ocs.spring.dao.hibernateImpl","com.ocs.spring.service"})
-@EnableWebMvc
+//@EnableWebMvc
 //@ImportResource("classpath:testContext.xml")
 public class AppConfig {
 }
